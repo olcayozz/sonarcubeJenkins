45b982b9be462620178cab03bf57eaf6b2692cc4
Olcay Ozyilmaz <olcayozz@gmail.com>
2022-12-18 00:24:49 +0300

    stop working services first

--- crontab.sh
+++ crontab.sh
@@ -1,6 +1,6 @@
 
-[ -d "/root/repo/sonarcubeJenkins/.git" ] && git --git-dir=/root/repo/sonarcubeJenkins/.git pull || git clone https://github.com/olcayozz/sonarcubeJenkins /root/repo/sonarcubeJenkins
 docker-compose -f /root/repo/sonarcubeJenkins/docker-compose.yml down
+[ -d "/root/repo/sonarcubeJenkins/.git" ] && git --git-dir=/root/repo/sonarcubeJenkins/.git pull || git clone https://github.com/olcayozz/sonarcubeJenkins /root/repo/sonarcubeJenkins
 docker-compose -f /root/repo/sonarcubeJenkins/docker-compose.yml up -d --build jenkins
 sleep 300
 docker exec -it jenkins_olcay sh /app/update_jenkins.sh
