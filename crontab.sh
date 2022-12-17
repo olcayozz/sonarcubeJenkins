f037786845ddb41283ba27d90d17d1512f607e18
Olcay Ozyilmaz <olcayozz@gmail.com>
2022-12-18 01:13:38 +0300

    crontab cleaned

--- crontab.sh
+++ crontab.sh
@@ -2,8 +2,14 @@
 
 
 docker-compose -f /root/repo/EXECUTION_REPO_sonarcubeJenkins/docker-compose.yml down
-[ -d "/root/repo/sonarcubeJenkins/.git" ] && git --git-dir=/root/repo/sonarcubeJenkins/.git pull & mkdir /root/repo/sonarcubeJenkins/services/jenkins/data & cp -r /jenkins /root/repo/sonarcubeJenkins/services/jenkins/data || git clone https://github.com/olcayozz/sonarcubeJenkins /root/repo/sonarcubeJenkins
-cp -rf /root/repo/sonarcubeJenkins /root/repo/EXECUTION_REPO_sonarcubeJenkins
+[ -d "/root/repo/sonarcubeJenkins/.git" ] && \
+(git --git-dir=/root/repo/sonarcubeJenkins/.git pull && \
+cp -rf /root/repo/sonarcubeJenkins /root/repo/EXECUTION_REPO_sonarcubeJenkins) || \
+(git clone https://github.com/olcayozz/sonarcubeJenkins /root/repo/sonarcubeJenkins && \
+cp -rf /root/repo/sonarcubeJenkins /root/repo/EXECUTION_REPO_sonarcubeJenkins && \
+rm -rf /root/repo/EXECUTION_REPO_sonarcubeJenkins/.git && \
+mkdir /root/repo/EXECUTION_REPO_sonarcubeJenkins/services/jenkins/data && \
+cp -r /jenkins /root/repo/EXECUTION_REPO_sonarcubeJenkins/services/jenkins/data/jenkins)
 docker-compose -f /root/repo/EXECUTION_REPO_sonarcubeJenkins/docker-compose.yml up -d --build jenkins
 sleep 300
 docker exec -it jenkins_olcay sh /app/update_jenkins.sh
