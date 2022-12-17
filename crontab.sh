6a7ba8720c5eb3a6a8a194b6816ce5b76bda242d
Olcay Ozyilmaz <olcayozz@gmail.com>
2022-12-18 00:02:55 +0300

    ports changed

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,8 @@
 
-docker-compose down
-git checkout
-docker-compose up -d --build jenkins
+[ -d "/root/repo/sonarcubeJenkins/.git" ] && git --git-dir=/root/repo/sonarcubeJenkins/.git pull || git clone https://github.com/olcayozz/sonarcubeJenkins /root/repo/sonarcubeJenkins
+docker-compose -f /root/repo/sonarcubeJenkins/docker-compose.yml down
+docker-compose -f /root/repo/sonarcubeJenkins/docker-compose.yml up -d --build jenkins
+sleep 300
 docker exec -it jenkins_olcay sh /app/update_jenkins.sh
 docker-compose up -d --build sonarqube
 
